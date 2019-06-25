2fce527bc8841a42348f064774341efb707f7382
Basttyy <basttyy@gmail.com>
2019-06-25 10:28:09 +0100

    Product Resource Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
