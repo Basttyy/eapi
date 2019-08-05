86ae31919d888d3a63b388291d6a9623e568fbaa
Basttyy <basttyy@gmail.com>
2019-08-05 15:44:09 +0100

    added product review creation

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    Protected $fillable = [
+        'star','customer','review'
+    ];
     public function product(){
         return $this->belongsTo(Product::class);
     }
