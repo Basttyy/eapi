340305c87eee68b1142e37854f706bc645af4504
Basttyy <basttyy@gmail.com>
2019-08-05 15:12:59 +0100

    added authorization for product update and delete

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -35,9 +35,9 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Product $product)
     {
-        //
+        return $product;
     }
 
     /**
